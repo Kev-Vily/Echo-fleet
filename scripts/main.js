055bc745daceb6ddf5e2a735061cc2a746b4d9ea
Blehhhh cat <artem.lukinykh.2009@gmail.com>
2025-11-06 11:05:20 +0200

    Create main.js

---
+++ scripts/main.js
@@ -0,0 +1,6 @@
+const exoprosopa = Vars.mods.locateMod("echo-fleet").meta;
+
+exoprosopa.displayName = Core.bundle.get("mod.echo-fleet.name");
+exoprosopa.author = Core.bundle.get("mod.echo-fleet.author");
+exoprosopa.description = Core.bundle.get("mod.echo-fleet.description");
+exoprosopa.subtitle = Core.bundle.get("mod.echo-fleet.subtitle"); 
